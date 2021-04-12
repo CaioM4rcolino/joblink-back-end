cd8be18d85da08334eeb4e79b32146aaf4540e2e
Gabriel Mochiute <Yukikuroshiro@users.noreply.github.com>
2021-04-12 11:57:41 -0300

    Adicionando o cors

--- src/app.js
+++ src/app.js
@@ -1,13 +1,20 @@
+const { errors } = require("celebrate");
 const express = require('express');
+
+const cors = require("cors");
+
 require('./database');
-
-
 
 const app = express();
 
 const routes = require('./routes');
 
 app.use(express.json());
+
+app.use(cors());
+
 app.use(routes);
+app.use(errors());
+
 
 module.exports = app;
