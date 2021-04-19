44a88f0a61452e69172f883199573c366976ce1b
Matheus Henrique <matheustennant@gmail.com>
2021-04-19 15:44:23 -0300

    Ajustando a rota de sessions

--- src/controllers/sessions.js
+++ src/controllers/sessions.js
@@ -1,78 +1,72 @@
-const Client = require('../models/Client');
-const Freelancer = require('../models/Freelancer');
-const bcrypt = require('bcryptjs');
+const Client = require("../models/Client");
+const User = require("../models/User");
+const Freelancer = require("../models/Freelancer");
+const bcrypt = require("bcryptjs");
 const { generateToken } = require("../utils");
 
-
 module.exports = {
+  async store(req, res) {
+    const { email, password } = req.body;
 
-    async store(req, res){
-        
-        const {email, password} = req.body;
+    try {
+      let token = "";
 
-        try {
+      const client = await User.findOne({
+        where: {
+          email,
+          is_freelancer: 0,
+        },
+      });
 
-            let token = "";
-            
-            const client = await Client.findOne({
-                where:{
-                    email
-                }
-            })
+      const freelancer = await User.findOne({
+        where: {
+          email,
+          is_freelancer: 1,
+        },
+      });
 
-            const freelancer = await Freelancer.findOne({
-                where:{
-                    email
-                }
-            })
+      if (!client || !bcrypt.compareSync(password, client.password)) {
+        if (!freelancer || !bcrypt.compareSync(password, freelancer.password)) {
+          return res
+            .status(403)
+            .send({ error: "Usuário e/ou senha inválidos" });
+        } else {
+          token = generateToken({
+            freelancerId: freelancer.id,
+            freelancerName: freelancer.name,
+          });
 
-            if(!client || !bcrypt.compareSync(password, client.password)){
-                if(!freelancer || !bcrypt.compareSync(password, freelancer.password)){
-                    return res.status(403).send({error: "Usuário e/ou senha inválidos"})
-                }
-                else{
+          res.status(201).send({
+            freelancer: {
+              name: freelancer.name,
+              email: freelancer.email,
+              birth_date: freelancer.birth_date,
+              cpf: freelancer.cpf,
+              years_experience: freelancer.years_experience,
+              history: freelancer.history,
+              rating: freelancer.rating,
+            },
 
-                    token = generateToken({freelancerId: freelancer.id, freelancerName: freelancer.name});
-
-                    res.status(201).send(
-                        {
-                            freelancer: {
-                                name: freelancer.name,
-                                email: freelancer.email,
-                                birth_date: freelancer.birth_date,
-                                cpf: freelancer.cpf,
-                                years_experience: freelancer.years_experience,
-                                history: freelancer.history,
-                                rating: freelancer.rating
-                            },
-                                
-                            token
-                    });
-
-                }
-            }
-            else{
-
-                token = generateToken({clientId: client.id, clientName: client.name});
-
-                res.status(201).send(
-                    {
-                        client: {
-                            name: client.name,
-                            email: client.email,
-                            birth_date: client.birth_date,
-                            cpf: client.cpf
-                        },
-                            
-                        token
-                });
-    
-            }
-
-        } catch (error) {
-            console.log(error);
-            res.status(500).send(error);
+            token,
+          });
         }
-    }, 
+      } else {
+        token = generateToken({ clientId: client.id, clientName: client.name });
 
-}
+        res.status(201).send({
+          client: {
+            name: client.name,
+            email: client.email,
+            birth_date: client.birth_date,
+            cpf: client.cpf,
+          },
+
+          token,
+        });
+      }
+    } catch (error) {
+      console.log(error);
+      res.status(500).send(error);
+    }
+  },
+};
