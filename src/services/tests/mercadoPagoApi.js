3962bef6bc5655a7cd9f8f71ee9c581d1245663f
CaioM4rcolino <caioaugustowill@gmail.com>
2021-05-12 12:04:38 -0300

    teste gitflow

--- src/services/tests/mercadoPagoApi.js
+++ src/services/tests/mercadoPagoApi.js
@@ -6,40 +6,40 @@ module.exports = {
     async createCostumer(req, res){
 
         // Adicione as credenciais
-        mercadopago.configure({
-            access_token: config.access_token
-        });
+        // mercadopago.configure({
+        //     access_token: config.access_token
+        // });
 
-        const {
-            first_name, 
-            last_name, 
-            email, 
-            phone, 
-            address, 
-            identification, 
-            data_registered, 
-            default_card
-        } = req.body;
+        // const {
+        //     first_name, 
+        //     last_name, 
+        //     email, 
+        //     phone, 
+        //     address, 
+        //     identification, 
+        //     data_registered, 
+        //     default_card
+        // } = req.body;
 
-        try {
+        // try {
 
-            const newCostumer = await mercadopago.post("/v1/customers", {
-                first_name,
-                last_name,
-                email,
-                phone,
-                address: Number(address),
-                identification,
-                data_registered,
-                default_card
-            })
+        //     const newCostumer = await mercadopago.post("/v1/customers", {
+        //         first_name,
+        //         last_name,
+        //         email,
+        //         phone,
+        //         address: Number(address),
+        //         identification,
+        //         data_registered,
+        //         default_card
+        //     })
     
-            return res.status(200).send(newCostumer);
+        //     return res.status(200).send(newCostumer);
 
-        } catch (error) {
-            console.log(error)
-            res.status(500).send(error)
-        }
+        // } catch (error) {
+        //     console.log(error)
+        //     res.status(500).send(error)
+        // }
 
     },
     
