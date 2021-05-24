ab07c5ab83898d24c240ff41d27fb77342bc033e
CaioM4rcolino <caioaugustowill@gmail.com>
2021-05-24 11:05:55 -0300

    rota de teste de preferencia ativa

--- src/services/testPreference.js
+++ src/services/testPreference.js
@@ -2,7 +2,7 @@ var mercadopago = require('mercadopago');
 const config = require("../config/mp-test-credentials.json");
 
 module.exports = {
-    async createPreference(){
+    async createPreference(req, res){
 
         //return console.log(mercadopago.preferences.schema.properties.collector_id)
 
@@ -10,41 +10,13 @@ module.exports = {
             access_token: config.access_token
         });
 
+        const {payer, shipments, payment_methods, items} = req.body;
+
         const request = {
-                payer:{
-                    properties:{
-                        name: "Teste",
-                        surname: "Etset",
-                        email: "teste@gmail.com",
-                        identification:{
-                            number: "47359807532"
-                        },
-                        phone: {
-                            area_code: "11",
-                            number: "98765-4321"
-                        },
-                        address: {
-                            street_name: "Calegari"
-                        }
-            
-                    }
-                },
-                payment_methods:{
-                    default_payment_method_id: "visa"
-                },
-               shipments:{
-                    receiver_address: {
-                        zip_code: "0655823",
-                        street_name: "Av. Sancheloma",
-                        street_number: 998
-                    }
-               },
-               items:[
-                   {
-                    quantity: 1,
-                    unit_price: 51
-                   }
-                ]
+                payer,
+                shipments,
+                payment_methods,
+                items
         }
 
         try {
