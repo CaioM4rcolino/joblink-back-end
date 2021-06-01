3c70df7e3f3441b26f78f07fb1c104cfe5b671db
CaioM4rcolino <caioaugustowill@gmail.com>
2021-05-31 21:47:27 -0300

    Rota que retorna freelancers próximos ao dono da requisição

--- src/services/googleMaps.js
+++ src/services/googleMaps.js
@@ -1,7 +1,7 @@
 const { Client } = require('@googlemaps/google-maps-services-js');
 const client = new Client({});
 const CONFIG = require("../config/maps-test-credentials.json");
-const { getPayload, validateModel } = require('../utils');
+const { getPayload, validateModel, haversine} = require('../utils');
 const User = require('../models/User');
 
 async function getFreelancersByLocation(req, res) {
@@ -13,14 +13,15 @@ async function getFreelancersByLocation(req, res) {
 
         const user = await validateModel(res, idUser, User, "Usuário");
 
+        //geocodificação do endereço de quem fez a requisição
         const geocode = await client.geocode({
             params: {
                 address: user.address,
                 key: CONFIG.access_key
             }
         })
-        //return res.status(200).send(geocode.data)
 
+        //varredura dos endereços de todos os freelancers do banco
         const allUsers = await User.findAll({
             attributes: ["id", "name", "address"],
             where: {
@@ -28,6 +29,7 @@ async function getFreelancersByLocation(req, res) {
             }
         })
 
+        //geocodificação do endereço de todos os freelancers do banco
         const promises = allUsers.map(async (u) => {
             return await client.geocode({
                 params: {
@@ -37,63 +39,36 @@ async function getFreelancersByLocation(req, res) {
             })
         })
 
+        //conclusão das promessas
         const response = await Promise.all(promises);
 
+        //varrer o array com todos os registros geocodificados, pegar as coordenadas de cada um e colocar na fórmula de haversine
         const nearAddress = response.map(r => {
+            const locationUser = geocode.data.results[0].geometry.location
+            const locationFreelancer = r.data.results[0].geometry.location;
+            const proximityCalc = haversine(locationUser, locationFreelancer)
 
-            const location = Object.values(r.data.results[0].geometry.bounds);
-            const meters = haversine(location);
-
-            if (meters < 100) {
-                return r.config.params.address;
-            }
-           
+            const formatted_address = r.data.results[0].formatted_address
         
+            const finalAddress = formatted_address.replace(", Brazil", "")
+
+            if(proximityCalc < 1000){
+                return finalAddress
+            }
         })
 
-        const findNearUser = allUsers.filter(u => nearAddress.find(nA => nA === u.address));
+            const findNearUser = allUsers.filter(u => nearAddress.find(nA => nA === u.address));
+            res.status(200).send(findNearUser)
+    
 
-        console.log(findNearUser)
-
-        // return console.log(resposta[0].data.results);
-        // const location = Object.values(geocode.data.results[0].geometry.bounds);
-
-        function haversine(location) {
-            const lat1 = location[0].lat;
-            const lat2 = location[1].lat;
-            const lon1 = location[0].lng;
-            const lon2 = location[1].lng;
-
-            const R = 6371e3; // metres
-            const φ1 = lat1 * Math.PI / 180; // φ, λ in radians
-            const φ2 = lat2 * Math.PI / 180;
-            const Δφ = (lat2 - lat1) * Math.PI / 180;
-            const Δλ = (lon2 - lon1) * Math.PI / 180;
-
-            const a = Math.sin(Δφ / 2) * Math.sin(Δφ / 2) +
-                Math.cos(φ1) * Math.cos(φ2) *
-                Math.sin(Δλ / 2) * Math.sin(Δλ / 2);
-            const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-
-            const d = R * c; // in metres
-
-            return d;
-
-
-            const locationObject = geocode.data.results[0].geometry.location;
-            const coordinates = `${location[0]},${location[1]}`;
-            return console.log([location, locationObject, coordinates])
-
-            // const placesNearby = await client.placesNearby({
-            //     params: {
-            //         location: locationObject,
-            //         radius: 1500,
-            //         type: "",
-            //         key: CONFIG.access_key
-            //     }
-            // })
-        }
-
+        // const placesNearby = await client.placesNearby({
+        //     params: {
+        //         location: locationObject,
+        //         radius: 1500,
+        //         type: "",
+        //         key: CONFIG.access_key
+        //     }
+        // })
 
 
     } catch (error) {
--- src/utils.js
+++ src/utils.js
@@ -31,4 +31,29 @@ const getPayload = (req) => {
 
 }
 
-module.exports = {generateToken, validateModel, getPayload};
+const haversine = (locationUser, locationFreelancer) => {
+
+    const lat1 = locationUser.lat;
+    const lat2 = locationFreelancer.lat;
+    const lon1 = locationUser.lng;
+    const lon2 = locationFreelancer.lng;
+
+    const R = 6371e3; // metres
+    const φ1 = lat1 * Math.PI / 180; // φ, λ in radians
+    const φ2 = lat2 * Math.PI / 180;
+    const Δφ = (lat2 - lat1) * Math.PI / 180;
+    const Δλ = (lon2 - lon1) * Math.PI / 180;
+
+    const a = Math.sin(Δφ / 2) * Math.sin(Δφ / 2) +
+                Math.cos(φ1) * Math.cos(φ2) *
+                Math.sin(Δλ / 2) * Math.sin(Δλ / 2);
+
+    const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+
+    const d = R * c; // in metres
+
+    return d;
+
+}
+
+module.exports = {generateToken, validateModel, getPayload, haversine};
